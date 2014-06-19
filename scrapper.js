3431be372e270f846077e77462e8fada6767cc98
Gabriel Seralha Miró <gsmiro@gmail.com>
2014-06-18 22:51:18 -0300

    Git ignore file and mongodb version.

--- scrapper.js
+++ scrapper.js
@@ -2,98 +2,84 @@ var htmlparser = require('htmlparser2');
 var https = require('https');
 var http = require('http');
 var fs = require('fs');
-function np(attrib,regexVal,cb){
-	var reg = new RegExp(regexVal);
-	var parser = new htmlparser.Parser({
-		onattribute:function(name,val){
-			if(name === attrib && reg.test(val)){
-				cb(val);		
-			}
-		}
-	});
-	return parser;
-}
-function get(base,val,cb,cache){
-	if(cache && cache[base+val]){
-		console.log('cached ' + base+val);
-		return cache[base+val];
-	}else console.log('requesting '+base+val)
-	var req = https.get(base + val,function(res){
-		console.log(base+val+' '+res.statusCode);
-		if(res.statusCode === 200){
-			res.pipe(typeof cb === 'Function'?cb(res):cb);
-		}else if(res.statusCode === 302){
-			get('',res.headers.location,cb,cache);
-		}else
-		  console.log(res); 
-	}).on('error',function(err){console.log(err)}).on('end',function(){
-		console.log('Closing stream '+base+val);
-		cb.end();
-	});;
-	if(cache) cache[base+val] = req;
-	return req;
-	
-}
+var crypto = require('crypto');
+var mongo = require('mongodb');
+
 var base = 'https://web.archive.org';
-var urls = {'/web/2014*/http://jw.org/en':true,'/web/2013*/http://jw.org/en':true,'/web/2012*/http://jw.org/en':true};
+var urls = {'https://web.archive.org/web/2014*/http://jw.org/en':true,'https://web.archive.org/web/2013*/http://jw.org/en':true,'https://web.archive.org/web/2012*/http://jw.org/en':true};
 var cache = {};
-function it(urls,regex,attr,endF){
+function it(base,urls,regex,attr,endF,process){
 	console.log(urls);
 	var reg = new RegExp(regex);
-	
+	var cnt = 0;
+	var  max = 0;
+	for( url in urls)max++;
+	var arr = {};
 	function _do(idx){
-		var arr = {};
 		var parser = new htmlparser.Parser({
 			onattribute:function(name,val){
 				if(name == attr && reg.test(val)){
-					console.log('read '+idx+' '+val);
-					arr[val] = true;
+					if(val.indexOf(base) == 0)
+						arr[val]=true;
+					else
+						arr[base+val] = true;
 				}
 			}
 		});
 		if(!cache[idx]){
 			console.log('cache miss '+idx);
-			var req = https.get(base+idx,function(res){
+			var req = https.get(idx,function(res){
 				res.on('data',function(data){
 					parser.write(data)
 				}).on('end',function(){
-					console.log('parsed '+this.req.path+' '+idx+ ' '+(this.req.path === idx));
-					console.log(arr);
+					console.log('parsed '+this.req.path+' for '+idx);
 					parser.end();
+					console.log(arr);
 					cache[this.req.path] = true;
-					endF(arr)
-				}).on('error',function(err){console.log(err)});
-			}).on('error',function(err){console.log(err)});
-		}else console.log('cached '+idx);
+					cnt++;
+					if(cnt == max)
+						endF(arr)
+					else console.log(cnt +' '+max);
+				}).on('error',function(err){console.log(err);cnt++;});
+			}).on('error',function(err){console.log(err);cnt++});
+		}else {console.log('cached '+idx);cnt++}
 	}
 	for( idx in urls ){
 		_do(idx);
 	}
 
 }
-it(urls,'.*http://www.jw.org/','href',function(arr){
-	it(arr,'.*assets/.*/[0-9]+.*(pnr|cnt).*lg.jpg','data-img-size-lg',function(arr){
+function persist(url){
+	var parts = url.split('/');
+	var fname = parts[parts.length-1];
+	if(!cache[url] && !cache[fname]){
+		var proto = /^https:\/\/.*/.test(url)?https:http;
+		return proto.get(url,function(res){
+			if(res.statusCode === 200){
+				if(!cache[url] && !cache[fname]){
+					cache[fname] = true;
+					var stream = fs.createWriteStream(fname);
+					stream.on('error',function(err){console.log(err);cache[fname]=false;}).on('finish',function(){console.log('wrote '+fname);});
+					res.pipe(stream);
+				}else console.log('cached '+url+' '+fname);
+			}else if(res.statusCode === 302){
+				console.log('REDIRECTING '+url+' to '+res.headers.location);
+				_get(res.headers.location);
+			}else console.log(res);
+		}).on('error',function(err){console.log(err)});
+	}else console.log('cached '+url+ ' '+fname);
+}
+
+it(base,urls,'.*http://www.jw.org/','href',function(arr){
+	it('http://assets.jw.org/',arr,'.*assets/.*/[0-9]+.*(pnr|cnt).*lg.jpg','data-img-size-lg',function(arr){
 		console.log(arr);
-		function _get(url){
-			var parts = url.split('/');
-			var fname = parts[parts.length-1];
-			if(!cache[url] && !cache[fname]){
-				var proto = /^https:\/\/.*/.test(url)?https:http;
-				return proto.get(url,function(res){
-					if(res.statusCode === 200){
-						if(!cache[url] && !cache[fname]){
-							cache[fname] = true;
-							var stream = fs.createWriteStream(fname);
-							stream.on('error',function(err){console.log(err);cache[fname]=false;}).on('finish',function(){console.log('wrote '+fname);});
-							res.pipe(stream);
-						}else console.log('cached '+url+' '+fname);
-					}else if(res.statusCode === 302){
-						console.log('REDIRECTING '+url+' to '+res.headers.location);
-						_get(res.headers.location);
-					}else console.log(res);
-				}).on('error',function(err){console.log(err)});
-			}else console.log('cached '+url+ ' '+fname);
-		}
-		for(url in arr)_get(url);
+		mongo.connect('mongodb://localhost:27017/jwscrapper',function(err,db){
+		  if(err)throw err;
+			var docs = [];
+			for(url in arr) docs.push({'_id':url});
+			db.collection('imgs').insert(docs,{w:1},function(err,result){
+				console.log(result);
+			});
+		});
 	});
 });
